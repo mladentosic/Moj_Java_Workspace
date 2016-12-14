f33d296bbc95ec0db33111cc81008d0144e7c085
mladentosic <mladen.tosic.tosa@gmail.com>
2016-12-14 14:05:41 +0100

    Nasljedjivanje1

--- Primjer21 - Nasljedjivanje/src/Car.java
+++ Primjer21 - Nasljedjivanje/src/Car.java
@@ -8,7 +8,9 @@ public class Car extends Machine {
     public void start() {
         System.out.println("Auto ukljuceno.");
     }
- 
+ /*
+  * Ako iskomentarisemo prethodnu funkciju, uzece start iz masina jer je car prosirenje masina a tamo ima start
+  */
     public void wipeWindShield() {
         System.out.println("Brisaci ukljuceni.");
     }
